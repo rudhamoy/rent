828e59117b3553746b045e475a5294f9456bfb6b
rudhamoy <rudhamoydebbarma@gmail.com>
2022-01-09 21:22:48 +0530

    apos fixed

--- components/hero/index.js
+++ components/hero/index.js
@@ -6,22 +6,21 @@ const HeroSection = () => {
         <div className={`${classes.background_svg} h-[65vh] sm:h-[85vh] `}>
             <div className="px-3 sm:px-32 backdrop-blur-[1.5px] h-full w-full flex flex-col justify-center pt-[20vh]">
                 <h1 className={`text-2xl text-[#241033] sm:text-6xl font-bold uppercase text-center ${classes.hero_heading}`}>Yes, Searching rent is tiring, Say no more!</h1>
-                <p className="my-2 text-center font-semibold text-gray-800">Let's find a perfect room for you</p>
+                <p className="my-2 text-center font-semibold text-gray-800">Let us find a perfect room for you</p>
 
                 {/** Search bar */}
                 <SearchBar />
                 {/** Example */}
-                <div className="max-w-sm sm:max-w-xl mx-auto">
-                    {/* <ul className="flex flex-wrap gap-x-2 font-semibold text-sm text-gray-800">
+                {/* <div className="max-w-sm sm:max-w-xl mx-auto">
+                    <ul className="flex flex-wrap gap-x-2 font-semibold text-sm text-gray-800">
                         <li>Example: </li>
                         <li>Abhoynagar,</li>
                         <li>Krishnanagar,</li>
                         <li>1bhk,</li>
                         <li>Family,</li>
                         <li>Students</li>
-                    </ul> */}
-
-                </div>
+                    </ul>
+                </div> */}
             </div>
         </div>
     )
