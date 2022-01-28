723591602ff374bb15e37bb5d7965da4ff30bf1e
rudhamoy <rudhamoydebbarma@gmail.com>
2022-01-28 18:53:39 +0530

    bookmark delete function

--- components/layout/room-card.js
+++ components/layout/room-card.js
@@ -6,16 +6,14 @@ import { ImLocation } from 'react-icons/im'
 import { FaRupeeSign } from 'react-icons/fa'
 import { BsFillBookmarkFill, BsBookmark, BsArrowRight } from 'react-icons/bs'
 import _ from 'lodash'
-import { WATCHLIST_ADD_ROOM } from "../../redux/constants/watchListConstants"
 import { DELETE_ROOM_RESET } from "../../redux/constants/roomConstants"
-import { addToWatchlist } from '../../redux/actions/watchListActions'
+import { addToWatchlist, getWatchList } from '../../redux/actions/watchListActions'
 import { toast } from 'react-toastify'
-import axios from 'axios'
 import { deleteRoom } from '../../redux/actions/roomActions'
 
 import { MdDelete, MdModeEdit } from 'react-icons/md'
 
-const RoomCard = ({ room, clicked, setShowModal, setShowRoom }) => {
+const RoomCard = ({ room, clicked, setShowModal, setShowRoom, bookmarkList }) => {
     const [loading, setLoading] = useState(false)
     const dispatch = useDispatch();
     const { error: deleteError, isDeleted } = useSelector(state => state.deleteRoom)
@@ -29,6 +27,7 @@ const RoomCard = ({ room, clicked, setShowModal, setShowRoom }) => {
 
     let watchList = []
 
+    // click handler for adding to watchlist/bookmark
     const handleAddToWatchlist = () => {
         if (typeof window !== "undefined") {
             if (localStorage.getItem("watchList")) {
@@ -44,14 +43,32 @@ const RoomCard = ({ room, clicked, setShowModal, setShowRoom }) => {
             localStorage.setItem("watchList", JSON.stringify(unique));
 
             dispatch(addToWatchlist(unique))
-            // dispatch({
-            //     type: WATCHLIST_ADD_ROOM,
-            //     payload: unique
-            // })
+
             toast.success("Added to Bookmarks")
         }
     }
 
+    //click handler for removing from wathclist
+    const removeFromWatchlist = (id) => {
+        if (typeof window !== "undefined") {
+
+            for (let i = 0; i < bookmarkList.length; i++) {
+                // let itemsList = JSON.parse(items[i]);
+                if (bookmarkList[i]._id === id) {
+                    bookmarkList.splice(i, 1);
+                }
+            }
+
+            localStorage.setItem("watchList", JSON.stringify(bookmarkList));
+
+            dispatch(addToWatchlist(bookmarkList))
+            dispatch(getWatchList())
+        }
+
+        console.log('clicked', id)
+    }
+
+    //show modal while user booking
     const showModalHandler = () => {
         setShowModal(true);
         setShowRoom(false)
@@ -64,6 +81,7 @@ const RoomCard = ({ room, clicked, setShowModal, setShowRoom }) => {
         localStorage.setItem("addBooking", JSON.stringify(addBooking))
     }
 
+    //delete room  for owner
     const deleteHandler = (id) => {
         if (window.confirm("Are you sure ?")) {
             dispatch(deleteRoom(id))
@@ -81,26 +99,6 @@ const RoomCard = ({ room, clicked, setShowModal, setShowRoom }) => {
         }
     }, [dispatch, deleteError, isDeleted, router])
 
-    // const newBooking = async () => {
-    //     const bookingData = {
-    //         room: room._id
-    //     }
-
-    //     try {
-    //         const config = {
-    //             headers: {
-    //                 "Content-Type": "application/json"
-    //             }
-    //         }
-
-    //         const { data } = await axios.post("/api/bookings", bookingData, config)
-
-    //         console.log(data)
-    //     } catch (error) {
-    //         console.log(error)
-    //     }
-    // }
-
 
     return (
         <div className={`flex flex-col gap-y-2 sm:gap-x-2 sm:flex-row bg-white ${pathname === '/' ? 'w-[75vw]' : 'w-[98%]'}  sm:w-[520px] h-[320px] sm:h-[174px] p-2 shadow-md rounded-md`}>
@@ -108,7 +106,7 @@ const RoomCard = ({ room, clicked, setShowModal, setShowRoom }) => {
             <div className="w-[100%] sm:w-[260px] relative">
                 <img src={images[0]} alt="" className="rounded-md h-[200px] sm:h-[165px] w-[100%]" />
                 <div className={`${pathname === "/me" || pathname === '/owner/room' ? 'hidden' : ""} absolute top-1 left-1 bg-[#00000066] p-2 rounded-full`}>
-                    {clicked === true ? <BsFillBookmarkFill className=" text-gray-50  text-lg cursor-pointer" /> : (
+                    {clicked === true ? <BsFillBookmarkFill onClick={() => removeFromWatchlist(room._id)} className=" text-gray-50  text-lg cursor-pointer" /> : (
                         <BsBookmark className={` text-gray-50  text-lg cursor-pointer`} onClick={handleAddToWatchlist} />
                     )}
                 </div>
--- pages/watch-list.js
+++ pages/watch-list.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { getWatchList } from '../redux/actions/watchListActions';
+import { addToWatchlist } from '../redux/actions/watchListActions'
 
 import RoomCard from '../components/layout/room-card';
 import Modal from '../components/layout/modal';
@@ -15,6 +16,24 @@ const WatchListPage = () => {
     const dispatch = useDispatch()
     const { rooms } = useSelector(state => state.allWatchList);
 
+    let bookmarkList = []
+    if (typeof window !== "undefined") {
+        if (localStorage.getItem("watchList")) {
+            bookmarkList = JSON.parse(localStorage.getItem("watchList"))
+        }
+    }
+    const removeAll = () => {
+        if (typeof window !== "undefined") {
+
+            localStorage.removeItem('watchList')
+
+            dispatch(addToWatchlist(bookmarkList))
+            dispatch(getWatchList())
+        }
+    }
+
+    console.log('bookmarkList', bookmarkList)
+
     useEffect(() => {
         dispatch(getWatchList())
     }, [dispatch])
@@ -23,8 +42,11 @@ const WatchListPage = () => {
     return (
         <div className='relative'>
 
-            <div className="pt-8 py-10 px-[3%] ">
-                <h1 className="sm:text-center sm:text-2xl font-semibold py-10 px-[3%]">My Bookmark List</h1>
+            <div className="pt-12 py-10 px-[3%] ">
+                <div className="py-10 px-[3%] flex justify-between">
+                    <h1 className="sm:text-center sm:text-2xl font-semibold ">My Bookmark List</h1>
+                    <button onClick={removeAll} className="text-[#512d6d]">clear all</button>
+                </div>
 
                 {rooms?.length === 0 && (
                     <div className="my-10 ">
@@ -32,15 +54,6 @@ const WatchListPage = () => {
                     </div>
                 )}
                 <div className="flex flex-col gap-y-5 justify-center items-center sm:my-10">
-                    {/* {showModal === true ? (
-                        <div className="absolute top-0 bottom-0 left-0 right-0 bg-[#00000066] z-50 flex justify-center py-[50%] ">
-                            <Modal setShowModal={setShowModal} setShowRoom={setShowRoom} />
-                        </div>
-                    ) :
-                        rooms && rooms.map(room => (
-                            <RoomCard room={room} key={room._id} clicked={clicked} setShowModal={setShowModal} setShowRoom={setShowRoom} />
-                        ))
-                    } */}
 
                     {showModal === true &&
                         <div className="absolute top-0 bottom-0 left-0 right-0 bg-[#00000066] z-50 flex justify-center py-[10%] ">
@@ -48,7 +61,9 @@ const WatchListPage = () => {
                         </div>
                     }
                     {rooms && rooms.map(room => (
-                        <RoomCard room={room} key={room._id} clicked={clicked} setShowModal={setShowModal} setShowRoom={setShowRoom} />
+                        <RoomCard room={room} key={room._id} clicked={clicked} setShowModal={setShowModal} setShowRoom={setShowRoom}
+                            bookmarkList={bookmarkList}
+                        />
                     ))
                     }
                 </div>
