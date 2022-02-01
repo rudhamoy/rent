4f416f6500146d88d6ea912f3d4ac1a3f5d4852c
rudhamoy <rudhamoydebbarma@gmail.com>
2022-02-01 18:30:48 +0530

    opitimizing for pc version

--- components/layout/index.js
+++ components/layout/index.js
@@ -1,16 +1,37 @@
+import { useEffect, useState } from 'react'
 import Header from '../header/index'
 import Footer from './footer'
 import MobileNav from './mobile-nav'
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css'
 import { useRouter } from 'next/router'
+import useWindowDimensions from './windowSize';
 
 const Layout = ({ children }) => {
-
+    const { height, width } = useWindowDimensions()
     const router = useRouter()
 
+    console.log(width)
+    if (width > 425) {
+        return (
+            <div className='flex justify-center items-center  py-24'>
+                <div>
+
+                    <h1 className="uppercase text-4xl font-semibold">Rentmeroom</h1>
+                    <div className="my-[20%]">
+
+                        <p>Currently, we are optimizing for pc version</p>
+                        <p>However, we are available at mobile version</p>
+                        <p>We request you to use your mobile device for browsing as of now!</p>
+                    </div>
+                    <h2 className="text-2xl text-yellow-700">Thank you!</h2>
+                </div>
+            </div>
+        )
+    }
+
     return (
-        <div className="bg-[#eeeeee] h-full relative">
+        <div className={`bg-[#eeeeee] h-full relative  `}>
             <Header />
             <ToastContainer position="top-right" />
             <div>
@@ -23,7 +44,7 @@ const Layout = ({ children }) => {
             <div className={`sticky bottom-[2px]`}>
                 <MobileNav />
             </div>
-        </div>
+        </div >
     )
 }
 
