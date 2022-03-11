da4548e7552f3a4eb1cba26a3e0578f0bb4738f1
rudhamoy <rudhamoydebbarma@gmail.com>
2022-03-11 14:11:06 +0530

    main database

--- next.config.js
+++ next.config.js
@@ -2,8 +2,8 @@ module.exports = {
   reactStrictMode: true,
   swcMinify: false,
   env: {
-    // MONGO_DB_URI: "mongodb+srv://rudhamoy:skateboard123@cluster0.zlick.mongodb.net/rentmeroom?retryWrites=true&w=majority",
-    MONGO_DB_URI: "mongodb+srv://yomahdur:skateboard123@bookit.cbr6i.mongodb.net/bookit?retryWrites=true&w=majority",
+    MONGO_DB_URI: "mongodb+srv://rudhamoy:skateboard123@cluster0.zlick.mongodb.net/rentmeroom?retryWrites=true&w=majority",
+    // MONGO_DB_URI: "mongodb+srv://yomahdur:skateboard123@bookit.cbr6i.mongodb.net/bookit?retryWrites=true&w=majority",
     CLOUDINARY_CLOUD_NAME: "yomah",
     CLOUDINARY_API_KEY: "677573329917884",
     CLOUDINARY_API_SECRET: "z9yP0mbkXFVE4t40EiXZpS0sYsM",
