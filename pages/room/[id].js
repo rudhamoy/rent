931ee604a015fba37c4e29a24b5e39beece21889
rudhamoy <rudhamoydebbarma@gmail.com>
2022-03-11 15:38:36 +0530

    stop single room

--- pages/room/[id].js
+++ pages/room/[id].js
@@ -1,44 +1,44 @@
-import RoomDetails from '../../components/room/room-detail';
-import Head from 'next/head'
-// import { getRoomDetails } from '../../redux/actions/roomActions';
-// import { wrapper } from '../../redux/store';
+// import RoomDetails from '../../components/room/room-detail';
+// import Head from 'next/head'
+// // import { getRoomDetails } from '../../redux/actions/roomActions';
+// // import { wrapper } from '../../redux/store';
 
-const RoomDetailPage = ({ room }) => {
-    return (
-        <>
-            <Head>
-                <title>{room.name}</title>
-                <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-            </Head>
-            <RoomDetails room={room} />
-        </>
-    )
-}
+// const RoomDetailPage = ({ room }) => {
+//     return (
+//         <>
+//             <Head>
+//                 <title>{room.name}</title>
+//                 <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+//             </Head>
+//             <RoomDetails room={room} />
+//         </>
+//     )
+// }
 
-// export const getServerSideProps = wrapper.getServerSideProps((store) => async ({ req, params }) => {
-//     await store.dispatch(getRoomDetails(req, params.id))
-// })
+// // export const getServerSideProps = wrapper.getServerSideProps((store) => async ({ req, params }) => {
+// //     await store.dispatch(getRoomDetails(req, params.id))
+// // })
 
-export async function getStaticPaths() {
-    const res = await fetch('https://rentmeroom.com/api/rooms')
-    const { rooms } = await res.json()
+// export async function getStaticPaths() {
+//     const res = await fetch('https://rentmeroom.com/api/rooms')
+//     const { rooms } = await res.json()
 
-    const paths = rooms.map((room) => ({
-        params: { id: room._id },
-    }))
+//     const paths = rooms.map((room) => ({
+//         params: { id: room._id },
+//     }))
 
-    return { paths, fallback: 'blocking' }
-}
+//     return { paths, fallback: 'blocking' }
+// }
 
 
-export async function getStaticProps({ params }) {
-    const res = await fetch(`https://rentmeroom.com/api/rooms/${params.id}`)
-    const { room } = await res.json()
+// export async function getStaticProps({ params }) {
+//     const res = await fetch(`https://rentmeroom.com/api/rooms/${params.id}`)
+//     const { room } = await res.json()
 
-    return {
-        props: { room },
-        revalidate: 1,
-    }
-}
+//     return {
+//         props: { room },
+//         revalidate: 1,
+//     }
+// }
 
-export default RoomDetailPage;
+// export default RoomDetailPage;
